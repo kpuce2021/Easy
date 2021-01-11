2d5e18f4aca06ec474008eec22bebbdc9606f6ed
Kim dong hyun <dhkim0405@naver.com>
2021-01-12 05:55:50 +0900

    Demo

--- app/src/main/java/com/mobileprogramming/videorecorderex/MainActivity.java
+++ app/src/main/java/com/mobileprogramming/videorecorderex/MainActivity.java
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    String mPath=null;  //동영상 녹화 파일 저장공간
+    MediaRecorder mRecorder=null;
     private Camera mCamera;
     private CameraPreview mPreview;
-
-
-
+    private boolean isRecording=false;  // 현재 녹화중인지 확인하기 위한 flag
+    Button recordButton;
+    int count=0;
     //===================================================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +40,72 @@ public class MainActivity extends AppCompatActivity {
             preview.addView(mPreview);
         }
 
+        recordButton=(Button)findViewById(R.id.button_capture);   // 버튼 클릭 리스너 정의
+        recordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                    startVideoRecord(); // recorder method 시작
+
+
+
+            }
+        });
 
     }
+    //===================================================================================================================
+    private void startVideoRecord(){
+        if(isRecording){
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder=null;
+
+            mCamera.lock();
+            isRecording=false;
+
+            recordButton.setText("녹화 시작");  // 버튼 텍스트 상태 변경
+
+        }else{
+            runOnUiThread(new Runnable(){
+                @Override
+                public void run() {
+                    mRecorder=new MediaRecorder();
+                    mCamera.unlock();   // 카메라 잠금 해제
+                    mRecorder.setCamera(mCamera);   // 카메라를 동영상 캡처에 사용하도록 설정
+                    //mRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);    //오디오 소스 설정
+                    mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA); // 동영상 소스 설정
+                    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);   // 동영상 출력 형식 설정
+                    mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);    //비디오 인코더 설정
+                    //mRecorder.setOrientationHint(90); //촬영 각도 맞추기
+
+
+                    mPath= Environment.getExternalStorageDirectory().getAbsolutePath()+"/record.mp4";
+
+                    // 저장 경로 설정
+                    mRecorder.setOutputFile(mPath);
+
+                    mRecorder.setPreviewDisplay(mPreview.previewHolder.getSurface());
+                    try{
+                        mRecorder.prepare();    //제공된 구성 설정으로 MediaRecorder 준비
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    mRecorder.start();
+                    isRecording=true;
+
+                    recordButton.setText("녹화중지");
+
+
+                }
+            });   // multi thread 구현
+
+
+        }
+
+    }
+
+
+
     //===================================================================================================================
     private boolean checkCameraHardware(Context context){   // 해당 디바이스가 카메라 하드웨어를 포함하고 있는지 체크하는 메소드 선언
         if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){    //카메라 하드웨어 있을 경우 true 리턴하게됨
@@ -85,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         try{    // preview change하기 전에 stop 구현
-            mCamera.stopPreview();
+            previewCamera.stopPreview();
         }catch(Exception e){
             // ignore: tried to stop a non-existent preview
         }
@@ -101,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-        // Take care of releasing the Camera preview in your activity.
+
     }
 }
 
