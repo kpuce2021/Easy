7c78a6413e530be3840b0b85c0c3e90c6b48c901
Kim dong hyun <dhkim0405@naver.com>
2021-03-10 02:25:32 +0900

    CascadeClassifier 차량 검출 구현 Tracer::CSRT 미구현 ROI 지정을 통한 연산 최소화 미구현 MediaRecord 구현 필요

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,4 @@
+//3월 2일 native
 
 #include <jni.h>
 #include <string>
@@ -5,8 +6,8 @@
 #include<opencv2/core/core.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
 #include<iostream>
-#define PI 3.141592
 
+#define PI 3.141592
 
 using namespace std;
 using namespace cv;
@@ -53,9 +54,7 @@ Java_com_mobileprogramming_twelve_MainActivity_ConvertImage(JNIEnv *env, jobject
     }else{
         count=0;
     }
-
     return count;
-
 }
 //====================================================================================
 bool find_left(Mat* frame, vector<Vec2f> lines){  //Min
@@ -116,6 +115,7 @@ void drawLines(Mat* frame, float rho, float theta){
     }
 }
 
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_mobileprogramming_twelve_MainActivity_alarmImage(JNIEnv *env, jobject thiz,
@@ -126,4 +126,53 @@ Java_com_mobileprogramming_twelve_MainActivity_alarmImage(JNIEnv *env, jobject t
     cvtColor(matInput, matInput, COLOR_BGR2HLS);
 
     // TODO: implement alarmImage()
+}
+
+
+
+
+//JNIEXPORT jlong JNICALL
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_com_mobileprogramming_twelve_MainActivity_loadCascade(JNIEnv *env, jobject thiz,
+                                                           jstring cascade_file_name) {
+    const char *nativeFileNameString = env->GetStringUTFChars(cascade_file_name, 0);
+
+    string baseDir("/storage/emulated/0/"); // cars.xml 파일 경로
+    baseDir.append(nativeFileNameString);
+    const char *pathDir = baseDir.c_str();
+
+    jlong ret = 0;
+    ret = (jlong) new CascadeClassifier(pathDir);
+    if (((CascadeClassifier *) ret)->empty()) {
+        //___android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
+          //                  "CascadeClassifier로 로딩 실패  %s", nativeFileNameString);
+    }
+    else
+        //__android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
+                            //"CascadeClassifier로 로딩 성공 %s", nativeFileNameString);
+    env->ReleaseStringUTFChars(cascade_file_name, nativeFileNameString);
+    return ret;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_mobileprogramming_twelve_MainActivity_detect(JNIEnv *env, jobject thiz,
+                                                      jlong cascade_classifier_car,
+                                                      jlong mat_addr_input, jlong mat_addr_result) {
+
+    Mat &img_input = *(Mat *) mat_addr_input;   //input frame
+    Mat &img_result = *(Mat *) mat_addr_result; //output frame
+
+    std::vector<Rect> cars;     //검출된 차량의 정보를 저장할 벡터 자료형
+
+    //((CascadeClassifier *) cascade_classifier_car)->detectMultiScale( img_resize, cars, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    ((CascadeClassifier *) cascade_classifier_car)->detectMultiScale(img_input, cars);  // 차량 검출 수행 -> 결과 cars벡터에 저장
+
+    if(!cars.empty()){
+        for(int i=0; i<cars.size(); i++){
+            rectangle(img_input, cars[i], Scalar(255,0,255),2);
+        }
+    }
 }
--- app/src/main/java/com/mobileprogramming/twelve/MainActivity.java
+++ app/src/main/java/com/mobileprogramming/twelve/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.mobileprogramming.twelve;
 
 import androidx.appcompat.app.AlertDialog;
@@ -7,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
@@ -26,10 +27,16 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
 
 import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static org.opencv.core.CvType.CV_8U;
 import static org.opencv.imgproc.Imgproc.Canny;
 import static org.opencv.imgproc.Imgproc.GaussianBlur;
@@ -40,16 +47,61 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     int count=0;
 
-    public native int ConvertImage(long matAddrInput, long matAddrResult, int count);
-    public native void alarmImage(long matAddrInput, long matAddrResult);
+    public native int ConvertImage(long matAddrInput, long matAddrResult, int count);   // 차선 검출 수행
+    public native void alarmImage(long matAddrInput, long matAddrResult);   // 이벤트 발생 시점 시각화
+    public native long loadCascade(String cascadeFileName);     // 스마트폰 내부 저장소에 저장한 cars.xml 파일의 경로 리턴
+    public native void detect(long cascadeClassifier_car, long matAddrInput, long matAddrResult);   // asset folder 내 cars.xml파일 기반 객체 검출 수행 및 frame위에 표시
+
     private static final String TAG = "opencv";
     private Mat matInput;
     private Mat matResult;
     private CameraBridgeViewBase mOpenCvCameraView;
+    long cascadeClassifier_car=0;
 
     static {
         System.loadLibrary("opencv_java4");
         System.loadLibrary("native-lib");
+    }
+
+    //=============================================================================================================
+    private void copyFile(String filename) {
+        String baseDir = Environment.getExternalStorageDirectory().getPath();   // 기본 저장 경로
+        String pathDir = baseDir + File.separator + filename;   // 기본 저장 경로 + cars.xml 파일의 경로
+
+        AssetManager assetManager = this.getAssets();   // asset folder내 cars.xml 파일 접근 하기 위한 assetmanager
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            Log.d( TAG, "copyFile :: 다음 경로로 파일복사 "+ pathDir);
+            inputStream = assetManager.open(filename);
+            outputStream = new FileOutputStream(pathDir);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+
+            inputStream.close();
+            inputStream = null;
+            outputStream.flush();
+            outputStream.close();
+            outputStream = null;
+        } catch (Exception e) {
+            Log.d(TAG, "copyFile :: 파일 복사 중 예외 발생 "+e.toString() ); //파일 복사 실패 시점에 유의
+        }
+
+    }
+    //=============================================================================================================
+    private void read_cascade_file(){   // native method loadCascade를 호출하여 cascadeClassifier_car 변수에 경로 저장
+
+        copyFile("cars.xml");
+        Log.d(TAG, "read_cascade_file");
+
+        cascadeClassifier_car=loadCascade("cars.xml");  //loadCascade -> native-lib.cpp 구현
+
     }
 
     //=============================================================================================================
@@ -74,7 +126,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
@@ -86,8 +137,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
-
-
 
     }
     //=============================================================================================================
@@ -131,26 +180,23 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     }
     @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {   // cvcameraviewframe => input
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {   // 프레임 단위 영상처리 수행
         matInput = inputFrame.rgba();
         Mat canny = new Mat(); // 원본 Mat canny;
         Mat Roi1, Roi2, Roi;
 
         if ( matResult == null ) {
-            matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
+            matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type()); // 연산 최소화를 위한 영역 지정
         }
-        count=ConvertImage(matInput.getNativeObjAddr(), matResult.getNativeObjAddr(), count);  // native-lib
+        count=ConvertImage(matInput.getNativeObjAddr(), matResult.getNativeObjAddr(), count);   // 차선 검출 메서드 호출 native-lib 구현
 
         if(count>18){
-            alarmImage(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());  // native-lib
+            alarmImage(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());  // 이벤트 발생 시점->화면전환
         }
-        // btn 위치
 
-//        int roi_x=50;   // pixel 따라 다름 수정필요
-//        int roi_y=2*(matInput.rows()/3);  // 하단 1/3 만 연산위해 영역 지정
-
+        detect(cascadeClassifier_car, matInput.getNativeObjAddr(), matResult.getNativeObjAddr()); // cars.xml 기반 차량 검출 메서드 호출
         return matInput;
-        // return result; //: edge 검출 완료
+
     }
     //=============================================================================================================
     protected List<? extends CameraBridgeViewBase> getCameraViewList() {
@@ -166,7 +212,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
         for (CameraBridgeViewBase cameraBridgeViewBase: cameraViews) {
             if (cameraBridgeViewBase != null) {
-                // cameraBridgeViewBase.setCameraPermissionGranted();
+                 //cameraBridgeViewBase.setCameraPermissionGranted();
+
+                read_cascade_file();
             }
         }
     }
@@ -176,8 +224,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onStart();
         boolean havePermission = true;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED||checkSelfPermission(WRITE_EXTERNAL_STORAGE)!= getPackageManager().PERMISSION_GRANTED) {
+                requestPermissions(new String[]{CAMERA, WRITE_EXTERNAL_STORAGE}, CAMERA_PERMISSION_REQUEST_CODE);
                 havePermission = false;
             }
         }
@@ -190,7 +238,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @TargetApi(Build.VERSION_CODES.M)
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_GRANTED) {
             onCameraPermissionGranted();
         }else{
             showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
@@ -207,7 +255,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         builder.setCancelable(false);
         builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id){
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+                requestPermissions(new String[]{CAMERA, WRITE_EXTERNAL_STORAGE}, CAMERA_PERMISSION_REQUEST_CODE);
             }
         });
         builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
@@ -219,6 +267,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     }
 
 }
+
 
 
 
