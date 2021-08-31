795722216fe4efbc46e8610ef54c0ba042540b35
DHKim <dhkim0405@naver.com>
2021-09-01 01:53:42 +0900

    opencv camera test - gray scale 적용

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_easydashcam_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+using namespace cv;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_easydashcam_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                           jlong mat_addr_input,
+                                                           jlong mat_addr_result) {
+    Mat &matInput = *(Mat *)mat_addr_input;
+    Mat &matResult = *(Mat *)mat_addr_result;
+
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 }
